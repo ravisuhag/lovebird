0b8cc9943c08d660ddfa034b785aec98cb45d5f5
Ravi Suhag <suhag.ravi@gmail.com>
2016-01-24 07:40:33 +0530

    Move to vorpal

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
 
-var program = require('commander');
+var vorpal = require('vorpal')();
+var excuses = require('./store/excuses');
+var chalk = require('chalk');
+var emoji = require('node-emoji');
 
-program
-    .version('0.0.1')
-    .command('excuse', 'Best excuse to get out of relationship.')
-    .command('compliment', 'Pay a compliment that can save you ass.')
-    .parse(process.argv);
+vorpal
+    .command('excuse', 'Best excuses to get out of relationship')
+    .action(function(args, callback) {
+        var excuse = excuses[getRandomInt(0, excuses.length)];
+        this.log(emoji.emojify(':broken_heart: :broken_heart: :broken_heart: :broken_heart: '), excuse);
+        callback();
+    });
+
+vorpal
+    .delimiter(chalk.bold.blue('lovebird$'))
+    .show();
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
